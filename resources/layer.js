0b8159eab38352c116c4a074ed5b184b0f6e46aa
unknown <thomas.pennamen@gmail.com>
2020-07-30 08:36:06 +0200

    >

--- resources/layer.js
+++ resources/layer.js
@@ -1,10 +1,14 @@
-const hydroSectors = require('../layers/SecteurHydro_FXX.json')
-const streamsRaw = require('../layers/paot_ctxt_cours_deau.json')
+// const hydroSectors = require('../layers/SecteurHydro_FXX.json')
+// const streamsRaw = require('../layers/paot_ctxt_cours_deau.json')
 
 module.exports.getHydroSector = (callback) => {
     return callback(hydroSectors.features)
 }
-module.exports.getStream = (callback) => {
-    const streamsFiltered = streamsRaw.features.filter(stream => stream.properties.classifica==1)
-    return callback(streamsFiltered)
+module.exports.getStream = ({query}, callback) => {
+    if (query.classifica) {
+        const streamsFiltered = streamsRaw.features.filter(stream => stream.properties.classifica==query.classifica)
+        return callback(streamsFiltered)
+    } else {
+        callback({})
+    }
 }
